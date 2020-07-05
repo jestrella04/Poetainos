689948f76a25b233cb2deac565f3d170e40a2ebb
Jonathan Estrella <jestrella04@gmail.com>
2020-07-05 12:26:25 -0400

    Enhancing look and feel of the offline page

--- resources/views/offline/index.blade.php
+++ resources/views/offline/index.blade.php
@@ -3,9 +3,9 @@
 @section('title', $params['title'])
 
 @section('body')
-<main role="main" class="container offline-wrapper">
+<main role="main" class="offline-wrapper">
     <div id="offline">
-        <img src="{{ mix('/static/images/logo.svg') }}" width="96" height="96">
+        <img src="{{ mix('/static/images/logo.svg') }}">
         <h1>{{ getSiteConfig('name') }}</h1>
         <p class="lead">{{ __('Oh no, it seems you are offline!') }}</p>
 
@@ -14,7 +14,10 @@
         </div>
 
         <div>
-            <a class="btn btn-primary btn-lg btn-block" href="{{ route('home') }}" role="button">{{ __('Reload') }}</a>
+            <a class="btn btn-primary btn-lg btn-block"
+                href="#"
+                onclick="location.reload()"
+                role="button">{{ __('Reload') }}</a>
         </div>
     </div>
 </main>
