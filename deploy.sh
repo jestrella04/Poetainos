269e9c601bd530f11de3c8efe149bb0c1a9afda1
Jonathan Estrella <jestrella04@gmail.com>
2020-07-27 21:45:37 -0400

    Updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
+
+# Turn on maintenance mode
 php artisan down
-git pull
-composer install --optimize-autoloader --no-dev
-yarnpkg install
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-php artisan optimize
-php artisan cache:clear
+
+# Pull the latest changes from the git repository
+# git reset --hard
+# git clean -df
+git pull origin master
+
+# Install/update composer dependecies
+composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
+
+# Run database migrations
 php artisan migrate --force
+
+# Clear caches
+php artisan cache:clear
+
+# Clear expired password reset tokens
+php artisan auth:clear-resets
+
+# Clear and cache routes
+php artisan route:clear
+php artisan route:cache
+
+# Clear and cache config
+php artisan config:clear
+php artisan config:cache
+
+# Clear and cache views
+php artisan view:clear
+php artisan view:
+
+# Restart service worker
 php artisan queue:restart
+
+# Install node modules
+yarnpkg install
+
+# Build assets using Laravel Mix
 yarnpkg run production
+
+# Turn off maintenance mode
 php artisan up
