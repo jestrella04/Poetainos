5f07e1564537b6fb1e6b43953915e42b92fc676b
Jonathan Estrella <jestrella04@gmail.com>
2021-08-05 20:46:54 -0400

    Fix likers visual issue

--- resources/views/writings/entry/likers.blade.php
+++ resources/views/writings/entry/likers.blade.php
@@ -6,14 +6,12 @@
     <div class="writing-likers">
         <small>{{ __('Liked by...') }}</small>
 
-        <div class="d-flex flex-wrap">
-            @foreach ($writing->likers()->inRandomOrder()->take(5)->get() as $liker)
-            <div class="author-link">
-                <a href="{{ $liker->path() }}" data-bs-toggle="tooltip" data-bs-placement="bottom" title="{{ $liker->getName() }}">
-                    {!! getUserAvatar($liker, $size = 'xl') !!}
-                </a>
-            </div>
-            @endforeach
+        @foreach ($writing->likers()->inRandomOrder()->take(5)->get() as $liker)
+        <div class="d-inline-flex author-link">
+            <a href="{{ $liker->path() }}" data-bs-toggle="tooltip" title="{{ $liker->getName() }}">
+                {!! getUserAvatar($liker, $size = 'xl') !!}
+            </a>
         </div>
+        @endforeach
     </div>
 @endif
