06d92ea6641351458a79ad88ebeba387cff2226f
Jonathan Estrella <jestrella04@gmail.com>
2023-05-15 23:00:23 -0400

    Misc UI

--- resources/views/notifications/partials/toplinks.blade.php
+++ resources/views/notifications/partials/toplinks.blade.php
@@ -1,21 +1,17 @@
-<div id="notification-top-links" class="top-links mb-3">
-    <div class="nav nav-tabs nav-fill">
-        <a @class([
-            'nav-item',
-            'nav-link',
-            'active' => Route::current()->getName() === 'notifications.list.unread',
-        ]) href="{{ route('notifications.list.unread') }}"
-        title="{{ __('Unread') }}">
-            {{ __('Unread') }}
-        </a>
+<nav class="nav nav-tabs nav-fill top-links mb-3">
+    <a @class([
+        'nav-link',
+        'active' => Route::current()->getName() === 'notifications.list.unread',
+    ]) href="{{ route('notifications.list.unread') }}"
+    title="{{ __('Unread') }}">
+        {{ __('Unread') }}
+    </a>
 
-        <a @class([
-            'nav-item',
-            'nav-link',
-            'active' => Route::current()->getName() ==='notifications.list.all',
-        ]) href="{{ route('notifications.list.all') }}"
-        title="{{ __('All') }}">
-            {{ __('All') }}
-        </a>
-    </div>
-</div>
+    <a @class([
+        'nav-link',
+        'active' => Route::current()->getName() ==='notifications.list.all',
+    ]) href="{{ route('notifications.list.all') }}"
+    title="{{ __('All') }}">
+        {{ __('All') }}
+    </a>
+</nav>
