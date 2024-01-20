bd406447f0035b30f2c47fb8ae1f041d3ebc3090
Jonathan Estrella <jestrella04@gmail.com>
2024-01-20 12:13:38 -0400

    Monochrome logo

--- logos.sh
+++ logos.sh
@@ -3,7 +3,7 @@
 # Set working directory
 cd public/images
 
-# Use Inkscape to generate PNGs from the SVG logo
+# Use Inkscape to generate PNGs from the SVG logos
 inkscape --export-background-opacity=0 --export-width=32 --export-type=png --export-filename=logo-32.png logo.svg
 inkscape --export-background-opacity=0 --export-width=64 --export-type=png --export-filename=logo-64.png logo.svg
 inkscape --export-background-opacity=0 --export-width=128 --export-type=png --export-filename=logo-128.png logo.svg
@@ -13,3 +13,4 @@ inkscape --export-background-opacity=0 --export-width=196 --export-type=png --ex
 inkscape --export-background-opacity=0 --export-width=256 --export-type=png --export-filename=logo-256.png logo.svg
 inkscape --export-background-opacity=0 --export-width=512 --export-type=png --export-filename=logo-512.png logo.svg
 inkscape --export-background-opacity=0 --export-width=512 --export-type=png --export-filename=logo-maskable.png logo-maskable.svg
+inkscape --export-background-opacity=0 --export-width=512 --export-type=png --export-filename=logo-mono.png logo-mono.svg
